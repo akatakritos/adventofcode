eff2c65b65633cb37332118ab9f1acc7507485fc
Matt Burke <git@mattburkedev.com>
2015-12-14 13:09:33 -0600

    wip on dinner table

---
+++ AdventOfCode.Core/DinnerTable.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Core
+{
+    internal struct HappinessPotential : IEquatable<HappinessPotential>
+    {
+        public string Name { get; }
+        public int HappinessDelta { get; }
+        public string Neighbor { get; }
+
+        public HappinessPotential(string name, int happinessDelta, string neighbor)
+        {
+            Name = name;
+            HappinessDelta = happinessDelta;
+            Neighbor = neighbor;
+        }
+
+        public static HappinessPotential Parse(string input)
+        {
+            var result = Regex.Match(input, @"^(?<name>\w+) would (?<verb>lose|gain) (?<change>\d+) happiness units by sitting next to (?<neighbor>\w+)\.$");
+
+            if (!result.Success)
+                throw new FormatException("Could not parse happiness line.");
+
+            var name = result.Groups["name"].Value;
+            var verb = result.Groups["verb"].Value;
+            var change = int.Parse(result.Groups["change"].Value);
+            var neighbor = result.Groups["neighbor"].Value;
+
+            return new HappinessPotential(
+                name,
+                verb == "lose" ? -1 * change : change,
+                neighbor);
+        }
+
+        public override string ToString()
+        {
+            var change = Math.Abs(HappinessDelta);
+            var verb = HappinessDelta < 0 ? "lose" : "gain";
+            return $"{Name} would {verb} {change} happiness units by sitting next to {Neighbor}.";
+        }
+
+        #region R# Equality Members
+
+        public bool Equals(HappinessPotential other)
+        {
+            return string.Equals(Name, other.Name) && HappinessDelta == other.HappinessDelta && string.Equals(Neighbor, other.Neighbor);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            return obj is HappinessPotential && Equals((HappinessPotential)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ HappinessDelta;
+                hashCode = (hashCode * 397) ^ (Neighbor != null ? Neighbor.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(HappinessPotential left, HappinessPotential right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(HappinessPotential left, HappinessPotential right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion
+    }
+
+    internal static class Permutator
+    {
+        public static void Permutate<T>(T[] input, Action<T[]> action)
+        {
+            PermutateImpl(input.ToArray(), action, 0, input.Length - 1);
+        }
+
+        private static void PermutateImpl<T>(T[] input, Action<T[]> action, int k, int m)
+        {
+            if (k == m)
+            {
+                action(input.ToArray());
+            }
+            else
+            {
+                for (var i = k; i <= m; i++)
+                {
+                    Swap(ref input[k], ref input[i]);
+                    PermutateImpl(input, action, k + 1, m);
+                    Swap(ref input[k], ref input[i]);
+                }
+            }
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+
+    public struct TableSeat
+    {
+        public string Name { get; }
+        public int LeftHappiness { get; }
+        public int RightHappiness { get; }
+    }
+
+    public class TableArrangement
+    {
+        public IEnumerable<TableSeat> TablePositions(string startingAt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DinnerTable
+    {
+        public void AddGuestRequirement(HappinessPotential parse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TableArrangement DetermineOptimalArrangement()
+        {
+
+        }
+    }
+}
---
+++ AdventOfCode.Tests/DinnerTableTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using AdventOfCode.Core;
+
+using NFluent;
+
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class DinnerTableTests
+    {
+        [Fact]
+        public void ParsesGainLine()
+        {
+            var result = HappinessPotential.Parse("Alice would gain 54 happiness units by sitting next to Bob.");
+
+            Check.That(result)
+                .IsEqualTo(new HappinessPotential("Alice", 54, "Bob"));
+        }
+
+        [Fact]
+        public void ParseLoseLine()
+        {
+            var result = HappinessPotential.Parse("Alice would lose 79 happiness units by sitting next to Carol.");
+
+            Check.That(result)
+                .IsEqualTo(new HappinessPotential("Alice", -79, "Carol"));
+        }
+    }
+
+    public class PermutatorTests
+    {
+        [Fact]
+        public void PermutatesAnyArray()
+        {
+            var input = new[] { 'A', 'B', 'C' };
+
+            var list = new List<string>();
+
+            Permutator.Permutate(input, element =>
+            {
+                list.Add(new string(element));
+            });
+
+            Check.That(list).Contains(
+                "ABC", "BCA", "BAC", "ACB", "CBA", "CAB");
+        }
+    }
+
+    public class DinnerTableTests
+    {
+        [Fact]
+        public void SampleProblem()
+        {
+            var subject = new DinnerTable();
+            subject.AddGuestRequirement(HappinessPotential.Parse("Alice would gain 54 happiness units by sitting next to Bob."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Alice would lose 79 happiness units by sitting next to Carol."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Alice would lose 2 happiness units by sitting next to David."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Bob would gain 83 happiness units by sitting next to Alice."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Bob would lose 7 happiness units by sitting next to Carol."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Bob would lose 63 happiness units by sitting next to David."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Carol would lose 62 happiness units by sitting next to Alice."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Carol would gain 60 happiness units by sitting next to Bob."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("Carol would gain 55 happiness units by sitting next to David."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("David would gain 46 happiness units by sitting next to Alice."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("David would lose 7 happiness units by sitting next to Bob."));
+            subject.AddGuestRequirement(HappinessPotential.Parse("David would gain 41 happiness units by sitting next to Carol."));
+
+            var result = subject.DetermineOptimalArrangement();
+
+            Check.That(result.TablePositions(startingAt: "David").Select(s => s.Name))
+                .ContainsExactly("David", "Alice", "Bob", "Carol");
+        }
+    }
+}
+
+
